ecff871f5c0125a2a6eb406917c486ff165eaf54
Hachmann <bjoern.hachmann@metrigo.de>
2016-08-24 16:07:44 +0200

    installing vundle

--- install-external-deps.sh
+++ install-external-deps.sh
@@ -4,3 +4,7 @@ mkdir ~/tools
 
 # install antigen
 cd ~/tools && git clone https://github.com/zsh-users/antigen.git
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
